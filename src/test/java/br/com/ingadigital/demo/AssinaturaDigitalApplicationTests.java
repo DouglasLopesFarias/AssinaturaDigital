8c47cbf8c6b6a9271a0d45b5b3348aaeb8236139
douglas <douglas@ingadigital.com.br>
2018-06-19 10:47:11 -0300

    Versão inicial do projeto

---
+++ src/test/java/br/com/ingadigital/demo/AssinaturaDigitalApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.ingadigital.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AssinaturaDigitalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
